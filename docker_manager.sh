fc2e5bbf361856b7acc4c7bd6a581570127da1bb
bastien_petitclerc <bastien.petitclerc@ecole2600.com>
2023-06-13 21:26:58 +0200

    start create

--- docker_manager.sh
+++ docker_manager.sh
@@ -6,10 +6,45 @@
 #   Created: 2023/05/25                                           #
 ###################################################################
 
-
-
-help()
+function script_help()
 {
+  echo -e "\nUsage: ./docker_manager.sh <action> [container | image] [numbers]"
+  echo -e "Exemple: ./docker_manager.sh --create alpine 5\n\noption:"
+  echo -e "  --create, -c\tCreate container(s) of image provided in arguments [container]"
+  echo -e "  --remove, -r\tDelete container(s) provided in arguements [container]"
+  echo -e "  --start, -i\tStart container(s) provided in arguments [container]"
+  echo -e "  --stop, -s\tStop container(s) provided in arguments [container]\n"
 
 }
+
+
+if [ "$1" == "--help" ] || [ "$1" == "-h" ];then
+  script_help
+elif [ "$1" == "--create" ] || [ "$1" == "-c" ];then
+  
+  image_name=""
+  container_number=1
+
+  if [[ -z "$2" ]];then
+      echo -e "need to specify an image name"
+  else
+      image_name="$2"
+  fi
+
+  if [[ "$3" != "" ]];then
+      container_number="$3"
+  fi
+
+
+  echo $container_number
+
+elif [ "$1" == "--remove" ] || [ "$1" == "-r" ];then
+  echo "remove"
+elif [ "$1" == "--start" ] || [ "$1" == "-i" ];then
+  echo "start"
+elif [ "$1" == "--stop" ] || [ "$1" == "-s" ];then
+  echo "stop"
+else
+  script_help
+fi
 
